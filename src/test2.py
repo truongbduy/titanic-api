a633ac413c09afeab801225f30dc161c617ec676
Duy Truong <duy.truong@tiki.vn>
2018-02-21 17:33:47 +0700

    Remove src folder

--- src/test2.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-print(os.path.dirname(__file__))
-print(os.path.join(os.path.dirname(__file__), '..'))
-#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-#import src
